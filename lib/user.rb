d7ed769a43249911eb9049c8434f83408d77b105
Mustafa Muse <umpp101@icloud.com>
2019-09-24 16:30:02 -0700

    Done.

---
+++ lib/user.rb
@@ -0,0 +1,9 @@
+class User
+    attr_reader :first_name, :last_name
+    attr_writer :first_name, :last_name
+    def initialize
+        @first_name = first_name
+        @last_name = last_name
+    end
+
+end
