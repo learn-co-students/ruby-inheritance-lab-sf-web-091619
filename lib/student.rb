2899d5684320bfe7d822373eebe0aad38a02a1c7
aryaziai <arya721@gmail.com>
2019-09-24 17:45:06 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,26 @@
-class Student 
+class Student < User
 
-end
+#     attr_reader :first_name, :last_name
+
+#     def initialize
+#         @first_name = first_name
+#         @last_name = last_name
+#     end
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(string)
+        @knowledge << string
+    end
+
+    def knowledge
+        @knowledge
+    end
+
+
+
+
+
+ end
---
+++ lib/user.rb
@@ -0,0 +1,14 @@
+require 'pry'
+
+class User
+
+attr_accessor :first_name, :last_name
+
+def initialize
+  
+end
+
+
+#binding.pry
+
+end
